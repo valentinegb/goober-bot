f775f3efa8b18e8f40440a1b78fc8726256ece28
Valentine Briese <valentinegb@icloud.com>
2024-06-16 22:34:42 -0700

    Add `/config get strikes_enabled`

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -17,7 +17,7 @@
 use anyhow::{bail, Context as _};
 use poise::{
     command,
-    serenity_prelude::{Color, CreateEmbed},
+    serenity_prelude::{Color, CreateEmbed, Timestamp},
     CreateReply,
 };
 use serde::{Deserialize, Serialize};
@@ -60,7 +60,7 @@ fn load_or_save_default_config(ctx: Context<'_>) -> Result<Config, Error> {
 /// Subcommands related to getting and setting server configuration
 #[command(
     slash_command,
-    subcommands("list"/*, "get", "set"*/),
+    subcommands("list", "get"/*, "set"*/),
     install_context = "Guild",
     interaction_context = "Guild",
     default_member_permissions = "MANAGE_GUILD"
@@ -70,7 +70,7 @@ pub(crate) async fn config(_ctx: Context<'_>) -> Result<(), Error> {
 }
 
 /// Lists all configuration options for this server
-#[command(slash_command)]
+#[command(slash_command, ephemeral)]
 async fn list(ctx: Context<'_>) -> Result<(), Error> {
     let config = load_or_save_default_config(ctx)?;
 
@@ -78,8 +78,37 @@ async fn list(ctx: Context<'_>) -> Result<(), Error> {
         CreateReply::default().embed(
             CreateEmbed::new()
                 .title("Configuration")
-                .description("These are the configuration options for this server.")
+                .description("These are the configuration options for this server. Use `/config get <option>` to get more information about an option.")
                 .field("Strikes Enabled", config.strikes_enabled.to_string(), false)
+                .timestamp(Timestamp::now())
+                .color(Color::BLUE),
+        ),
+    )
+    .await?;
+
+    Ok(())
+}
+
+/// Gets a specific configuration option
+#[command(slash_command, subcommands("get_strikes_enabled"))]
+async fn get(_ctx: Context<'_>) -> Result<(), Error> {
+    unreachable!()
+}
+
+/// Gets the Strikes Enabled configuration option
+#[command(slash_command, rename = "strikes_enabled", ephemeral)]
+async fn get_strikes_enabled(ctx: Context<'_>) -> Result<(), Error> {
+    let Config {
+        strikes_enabled, ..
+    } = load_or_save_default_config(ctx)?;
+
+    ctx.send(
+        CreateReply::default().embed(
+            CreateEmbed::new()
+                .title("Strikes Enabled")
+                .description("Whether to enable the strikes moderation system, `/strike`, and its subcommands")
+                .field("Value", strikes_enabled.to_string(), false)
+                .timestamp(Timestamp::now())
                 .color(Color::BLUE),
         ),
     )
