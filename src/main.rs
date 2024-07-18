c928a743fcba41e4672da9e70b8c458c1f6c38cc
Valentine Briese <valentinegb@icloud.com>
2024-07-17 17:46:30 -0700

    Make TODO note about current priority

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// TODO: make general refinements to existing codebase
 // TODO: add birthday announcements system
 
 mod activity;
