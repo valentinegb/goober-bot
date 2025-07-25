f722754b9343c0f37c05431eee4b1ea397142e1e
Valentine Briese <valentinegb@icloud.com>
2025-07-25 12:08:29 -0700

    Initialize Nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+
+      packages =
+        nixpkgs.lib.genAttrs (nixpkgs.lib.remove "x86_64-freebsd" nixpkgs.lib.systems.flakeExposed)
+          (system: {
+            goober-bot = nixpkgs.legacyPackages.${system}.rustPlatform.buildRustPackage {
+              pname = "goober-bot";
+              version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+            };
+
+            default = self.packages.${system}.goober-bot;
+          });
+
+    };
+}
